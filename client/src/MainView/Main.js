01c3d7b0056309a83b2cbccc7fe29d77f0f948bd
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-11-05 10:27:28 -0600

    Loading user recipes along with a fixed navbar. Ugly, but it works

--- client/src/MainView/Main.js
+++ client/src/MainView/Main.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import MainCookbook from './MainCookbook';
-// import NavBar from '../Layout/NavBar';
+import NavBar from '../Layout/NavBar';
 
 export default class Main extends Component {
 
 
 	render() {
 		return (
-			<React.Fragment>
-				{/* <NavBar /> */}
+			<div>
+				<NavBar />
 				<MainCookbook />
-			</React.Fragment>
+			</div>
 		)
 	}
 }
--- client/src/MainView/MainCookbook.js
+++ client/src/MainView/MainCookbook.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, SegmentGroup } from 'semantic-ui-react';
+import { Container, SegmentGroup, Header} from 'semantic-ui-react';
 import MainRecipe from './MainRecipe';
 import ApiMethods from '../API/ApiMethods';
 
@@ -21,6 +21,9 @@ export default class MainCookbook extends Component {
 	render() {
 		return (
 			<Container>
+				<Header size="huge"></Header>
+				<Header size="huge"></Header>
+				<Header size="huge">Your Cookbook</Header>
 				<SegmentGroup>
 					{this.state.recipeList.map(recipe => {
 						return <MainRecipe recipe={recipe} key={recipe.recipeId} />
