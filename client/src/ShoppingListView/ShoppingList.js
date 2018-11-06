22509716991b56824bf21583e3ebaf5b4d5dd8b6
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-11-06 12:10:39 -0600

    Checking off ingredients deactivates them and vice versa. I think this ticket is finished!

--- client/src/ShoppingListView/ShoppingList.js
+++ client/src/ShoppingListView/ShoppingList.js
@@ -9,10 +9,23 @@ export default class ShoppingList extends Component {
 		ingredientList: [{}]
 	}
 
+	toggleIngredient = (evt) => {
+		const ingredientIndex = evt.target.id.split('-')[2];
+		const updatedIngredients = this.state.ingredientList;
+		updatedIngredients[ingredientIndex].checked = !this.state.ingredientList[ingredientIndex].checked;
+
+		this.setState(() => {
+			return {ingredientList: updatedIngredients}
+		})
+	}
+
 	componentDidMount() {
 		ApiMethods.getShoppingList()
 			.then(ingredientsArray =>
 				this.setState(() => {
+					ingredientsArray.forEach(ingredient => {
+						ingredient.checked = false;
+					})
 					return { ingredientList: ingredientsArray }
 				})
 			)
@@ -25,9 +38,10 @@ export default class ShoppingList extends Component {
 				<Header size="huge"></Header>
 				<Header size="huge">Shopping List</Header>
 
-				<List>
+				<List size='massive'>
 					{this.state.ingredientList.map((ingredient, i) => {
-						return <ShoppingListIngredient index={i} ingredient={ingredient} key={ingredient.ingredientId} toggleIngredient={(evt) => { this.toggleingredient(evt) }} />
+						return <ShoppingListIngredient index={i} ingredient={ingredient} key={ingredient.ingredientId}
+						toggleIngredient={(evt) => { this.toggleIngredient(evt) }} />
 					})}
 				</List>
 			</Container>
--- client/src/ShoppingListView/ShoppingListIngredient.js
+++ client/src/ShoppingListView/ShoppingListIngredient.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
-import {List} from 'semantic-ui-react'
+import { List, Checkbox } from 'semantic-ui-react'
+import './List.css'
 
 export default class ShoppingListIngredient extends Component {
 
 	render() {
+
+		const checkedIngredient = this.props.ingredient.checked ? 'checkedIngredient' : 'uncheckedIngredient';
+
 		return (
-			<List.Item>
-				<List.Icon name='box' />
-				<List.Content>{`${this.props.ingredient.quantity} ${this.props.ingredient.food}`}</List.Content>
+			<List.Item size='huge'>
+				<List.Content size='huge'>
+					<Checkbox id={`checkbox-${this.props.ingredient.id}-${this.props.index}`}
+					size='huge'
+						checked={this.props.ingredient.checked}
+						className={checkedIngredient}
+						label={`${this.props.ingredient.food} (${this.props.ingredient.quantity})`}
+						onChange={this.props.toggleIngredient} />
+				</List.Content>
 			</List.Item>
 		)
 	}
